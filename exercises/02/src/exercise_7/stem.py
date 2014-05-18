e3a02e07947bf5b95d033a570a67a68623088f27
Simon Schneegans <code@simonschneegans.de>
2014-05-18 18:46:14 +0200

    solved exercise 7

---
+++ exercises/02/src/exercise_7/stem.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import Stemmer
+from sets import Set
+import re
+
+def stem(algo):
+    file=open(sys.argv[1],"r+")
+    stems=Set()
+    stemmer = Stemmer.Stemmer(algo)
+    total_count = 0
+
+    for w in file.read().split():
+        word = w.lower()
+        word = re.sub("[\, \- \. \" \; \! \* \? \( \) \:]+", "", word.strip())
+        if len(word) > 0:
+            stem = stemmer.stemWord(word)
+            stems.add(stem)
+            total_count += 1
+
+    file.close();
+
+    print algo, len(stems), total_count
+
+
+stem('english')
+stem('porter')
