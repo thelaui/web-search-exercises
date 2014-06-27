18f7ea32559202e239d4d4eecd5ed7ec13594a71
Felix Lauer <felix.lauer@uni-weimar.de>
2014-06-27 10:23:23 +0200

    added solution for exercise 3

--- exercises/02/src/exercise_7/stem.py
+++ exercises/02/src/exercise_7/stem.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python
 
 import sys
-import Stemmer
+#import Stemmer
 from sets import Set
 import re
 
 def stem(algo):
     file=open(sys.argv[1],"r+")
     stems=Set()
-    stemmer = Stemmer.Stemmer(algo)
+    #stemmer = Stemmer.Stemmer(algo)
     total_count = 0
 
     for w in file.read().split():
         word = w.lower()
         word = re.sub("[\, \- \. \" \; \! \* \? \( \) \:]+", "", word.strip())
         if len(word) > 0:
-            stem = stemmer.stemWord(word)
-            
-            print stem
-            
-            stems.add(stem)
+            #stem = stemmer.stemWord(word)
+
+            print word
+
+            stems.add(word)
             total_count += 1
 
     file.close();
-    
+
     #for stem in stems:
     #    print stem
-        
+
     print algo, len(stems), total_count
 
 
